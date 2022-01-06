456f6fd7a278356636f7251ca21620c5d8c084a9
EricRicketts <eric_ricketts@me.com>
2022-01-06 10:32:41 -0500

    Lesson 3 Challenge Problems

---
+++ TeachYourselfSQLInTenMinutes/TeachYourselfSQL_MySQL/Lesson2/challenges/problem_four.sql
@@ -0,0 +1,5 @@
+# SELECT vend_name, should not be a comma here
+SELECT vend_name
+FROM Vendors
+# ORDER vend_name DESC; needs ORDER BY
+ORDER BY vend_name DESC;
---
+++ TeachYourselfSQLInTenMinutes/TeachYourselfSQL_PostgreSQL/Lesson2/challenges/problem_one.sql
@@ -0,0 +1,1 @@
+SELECT cust_name FROM Customers ORDER BY cust_name DESC; 
